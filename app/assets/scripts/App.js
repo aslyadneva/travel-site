676293363361dd16f7aeb0ef5f6fe3ef3a951895
Anastasia <slyadneva.anastasia@gmail.com>
2020-05-23 21:51:13 -0500

    Completed desktop and mobile header styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,17 @@
+import MobileMenu from './modules/MobileMenu'
+
 import '../styles/styles.css'
+
+const mobileMenu = new MobileMenu(); 
+
+
+
+
+
+
+
 
 if (module.hot) {
   module.hot.accept()
 }
+

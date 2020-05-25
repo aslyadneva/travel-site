d2bdcec303abc177199cf948d65760899df97307
Anastasia <slyadneva.anastasia@gmail.com>
2020-05-25 14:46:17 -0500

    Completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 import '../styles/styles.css'
 
-const mobileMenu = new MobileMenu(); 
+let stickyHeader = new StickyHeader()
+const mobileMenu = new MobileMenu()
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75); 
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60); 

53241ca3cfe794de68fd6e92128522fbef797b86
Anastasia <slyadneva.anastasia@gmail.com>
2020-05-25 20:47:07 -0500

    Completed modal style and function

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,15 +4,28 @@ import StickyHeader from './modules/StickyHeader';
 
 import '../styles/styles.css'
 
-let stickyHeader = new StickyHeader()
-const mobileMenu = new MobileMenu()
-
+new StickyHeader()
+new MobileMenu()
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75); 
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60); 
+let modal 
 
+// ONLY DOWNLOADING THE MODAL BUNDLE IF USER CLICKS 'GET IN TOUCH' --- CODE SPLITTING 
+document.querySelectorAll('.open-moodal').forEach(btn => btn.addEventListener('click', event => {
+  event.preventDefault()
 
-
-
+  if (typeof modal === 'undefined') {
+    import(/* webpackChunkName: "modal" */'./modules/Modal') // this returns a promise 
+    .then(file => {
+      modal = new file.default()
+      setTimeout(() => modal.openModal(), 20)
+    }) 
+    .catch(() => console.log('There was a problem'))
+  } else {
+    modal.openModal()
+  }
+  
+}))
 
 
 
